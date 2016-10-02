38b1862a55c2fba5c28064ffebb7e00424b1a662
Oleg Parashchenko <olpa@uucode.com>
2016-10-02 11:48:03 +0200

    Calibrate instantiates.

--- test-calibrate.cpp
+++ test-calibrate.cpp
@@ -1,5 +1,13 @@
 #include <gmock/gmock.h>
+
+class Calibrate {
+};
+
 using namespace testing;
+
+TEST(Calibrate, Instaltiates) {
+  Calibrate c;
+}
 
 int main(int argc, char**argv) {
   InitGoogleMock(&argc, argv);
