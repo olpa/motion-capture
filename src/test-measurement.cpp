99fafa85c3033856a2781aae8d69011a18e2898d
Oleg Parashchenko <olpa@uucode.com>
2016-10-03 16:50:24 +0200

    Newly instantiated measurement is all-zero.

--- src/test-measurement.cpp
+++ src/test-measurement.cpp
@@ -1,17 +1,31 @@
-#include "measurement.hpp"
 #include <gmock/gmock.h>
 
 using namespace testing;
 
-TEST(Measurement, Instaltiates) {
+struct Measurement {
+  Measurement() : x(0), y(0), z(0), ax(0), ay(0), az(0) { };
+  int x, y, z, ax, ay, az;
+};
+
+TEST(Measurement, Instantiates) {
   Measurement m;
 }
 
-TEST(Measurement, CanCompare) {
-  Measurement m1, m2;
-  ASSERT_THAT(m1, Eq(m2));
-  // Not eq
+TEST(Measurement, InstantiatesEmpty) {
+  Measurement m;
+  ASSERT_THAT(m.x, Eq(0));
+  ASSERT_THAT(m.y, Eq(0));
+  ASSERT_THAT(m.z, Eq(0));
+  ASSERT_THAT(m.ax, Eq(0));
+  ASSERT_THAT(m.ay, Eq(0));
+  ASSERT_THAT(m.az, Eq(0));
 }
+
+//TEST(Measurement, CanEqualCompare) {
+//  Measurement m1, m2;
+//  ASSERT_THAT(m1, Eq(m2));
+//  // Not eq
+//}
 
 int main(int argc, char**argv) {
   InitGoogleMock(&argc, argv);
