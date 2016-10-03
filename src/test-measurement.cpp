bab4d7d8af8ce098759df74ae6f25c0558a3c9c7
Oleg Parashchenko <olpa@uucode.com>
2016-10-03 16:58:01 +0200

    Compare operator for measurements.

--- src/test-measurement.cpp
+++ src/test-measurement.cpp
@@ -5,6 +5,11 @@ using namespace testing;
 struct Measurement {
   Measurement() : x(0), y(0), z(0), ax(0), ay(0), az(0) { };
   int x, y, z, ax, ay, az;
+
+  bool operator==(Measurement const& rhs) const {
+    return (x == rhs.x) && (y == rhs.y) && (z == rhs.z)
+      && (ax == rhs.ax) && (ay == rhs.ay) && (az == rhs.az);
+  }
 };
 
 TEST(Measurement, Instantiates) {
@@ -21,11 +26,12 @@ TEST(Measurement, InstantiatesEmpty) {
   ASSERT_THAT(m.az, Eq(0));
 }
 
-//TEST(Measurement, CanEqualCompare) {
-//  Measurement m1, m2;
-//  ASSERT_THAT(m1, Eq(m2));
-//  // Not eq
-//}
+TEST(Measurement, EqualCompare) {
+  Measurement m1, m2;
+  m1.x = 7;
+  m2.x = 7;
+  ASSERT_THAT(m1, Eq(m2));
+}
 
 int main(int argc, char**argv) {
   InitGoogleMock(&argc, argv);
