e0a157d470b6bf52ca1048cbe8986b6a3a8dc53b
Oleg Parashchenko <olpa@uucode.com>
2016-10-04 14:32:19 +0200

    Read and write also temperature.

--- src/if_iostream.cpp
+++ src/if_iostream.cpp
@@ -2,10 +2,10 @@
 
 Measurement measure(std::istream& is) {
   Measurement m;
-  is >> std::hex >> m.x >> m.y >> m.z >> m.ax >> m.ay >> m.az >> std::dec;
+  is >> std::hex >> m.x >> m.y >> m.z >> m.ax >> m.ay >> m.az >> m.temperature >> std::dec;
   return m;
 }
 
 std::ostream& operator<<(std::ostream& os, Measurement const& m) {
-  os << std::hex << m.x << ' ' << m.y << ' ' << m.z << ' ' << m.ax << ' ' << m.ay << ' ' << m.az << std::dec;
+  os << std::hex << m.x << ' ' << m.y << ' ' << m.z << ' ' << m.ax << ' ' << m.ay << ' ' << m.az << ' ' << m.temperature << std::dec;
 }
