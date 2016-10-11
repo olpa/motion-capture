3b805a2dca96b14f5fb7957875f576647f6ad4df
Oleg Parashchenko <olpa@uucode.com>
2016-10-11 17:44:57 +0200

    New device-file based interface used.

--- src/mpu.cpp
+++ src/mpu.cpp
@@ -1,48 +1,27 @@
-#include <fstream>
 #include <iostream>
-#include <ios>
-#include <iterator>
 #include <system_error>
 #include <string>
-#include <cstring>
-#include <cassert>
-#include <cstdint>
-#include <fcntl.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
 
 #include "measurement.hpp"
+#include "if_mpu6050.hpp"
+#include "if_iostream.hpp"
 
-const std::string icsfile("/dev/i2c-1");
-constexpr int mpuaddr{0x68};
-constexpr int reg{67};
+const std::string i2c_device_file("/dev/i2c-1");
+constexpr int mpu_address{0x68};
 
 int main(int args, char** argv) {
-  std::fstream fdev;
-  int hdev = ::open("/dev/null", 0);
-  if (-1 == hdev) {
-    std::cerr << "Checking /dev/null: " << ::strerror(errno) << std::endl;
-    return 1;
-  }
-  assert(! ::close(hdev)); // The handle will be reused
-  fdev.exceptions(std::ios::failbit | std::ios::badbit);
   try {
-    fdev.open(icsfile);
-  } catch (std::ios_base::failure &e) {
-    std::cerr << icsfile << ": " << ::strerror(errno) << std::endl;
-    return 1;
-  }
-  if (::ioctl (hdev, I2C_SLAVE, mpuaddr) < 0) {
-    std::cerr << "ioctl failed: " << ::strerror(errno) << std::endl;
-    return 1;
-  }
-  Measurement m;
-  for (int i = 0; i < 10; i++) {
-    m.measure(hdev);
-    std::cout << m << std::endl;
-    ::sleep(1);
+    i2c_Device_File hdev(i2c_device_file, mpu_address);
+    Measurement m;
+    for (int i = 0; i < 10; i++) {
+      m = measure(hdev);
+      std::cout << m << std::endl;
+      ::sleep(1);
+    }
+  } catch (std::system_error &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return -1;
   }
   return 0;
 }
