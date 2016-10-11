5c71af807580da6f29163430b8efbaed1849bb6c
Oleg Parashchenko <olpa@uucode.com>
2016-10-11 14:47:30 +0200

    Added: read word.

--- src/i2c_lib.cpp
+++ src/i2c_lib.cpp
@@ -5,6 +5,7 @@
 //
 // Copy-paste from i2ctools, GPL 2
 // http://www.lm-sensors.org/
+// https://github.com/groeck/i2c-tools
 //
 __s32 i2c_smbus_access(int file, char read_write, __u8 command,
     int size, union i2c_smbus_data *data)
@@ -33,4 +34,24 @@ __s32 i2c_smbus_read_byte_data(int file, __u8 command)
     return err;
 
   return 0x0FF & data.byte;
+}
+
+__s32 i2c_smbus_read_word_data(int file, __u8 command)
+{
+  union i2c_smbus_data data;
+  int err;
+
+  err = i2c_smbus_access(file, I2C_SMBUS_READ, command,
+      I2C_SMBUS_WORD_DATA, &data);
+  if (err < 0)
+    return err;
+
+  return 0x0FFFF & data.word;
+}
+
+//
+// My
+//
+int i2c_set_slave_address(int handle, int addr) {
+  return ::ioctl(handle, I2C_SLAVE, addr);
 }
