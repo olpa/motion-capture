c2cf6026dcc6ba33a9639cfa3b6fe28d320e403f
olpa <olpa@uucode.com>
2016-10-19 15:21:56 +0200

    IMU events sent.

--- ros/send_imu_events.py
+++ ros/send_imu_events.py
@@ -1,14 +1,32 @@
 import rospy
 import std_msgs.msg
+import sensor_msgs.msg
+import QuatWithEffect
+
+def set_quat(obj, vals):
+  (obj.x, obj.y, obj.z, obj.w) = vals
+
+def set_vector3(obj, vals):
+  (obj.x, obj.y, obj.z) = vals
 
 def talker():
-  pub = rospy.Publisher('chatter', std_msgs.msg.String, queue_size=10)
-  rospy.init_node('talker', anonymous=True)
+  pub = rospy.Publisher('fake_imu', sensor_msgs.msg.Imu, queue_size=10)
+  rospy.init_node('fake_imu', anonymous=True)
   rate = rospy.Rate(10) # 10hz
+  quat = QuatWithEffect.QuatWithEffect()
   while not rospy.is_shutdown():
-    hello_str = "hello world %s" % rospy.get_time()
-    rospy.loginfo(hello_str)
-    pub.publish(hello_str)
+    msg = sensor_msgs.msg.Imu()
+    set_quat(msg.orientation, quat.next())
+    msg.orientation_covariance = [
+      1.0, 0.0, 0.0,
+      0.0, 1.0, 0.0,
+      0.0, 0.0, 1.0 ]
+    set_vector3(msg.angular_velocity,    (0, 0, 0))
+    set_vector3(msg.linear_acceleration, (0, 0, 0))
+    msg.angular_velocity_covariance    = (0, 0, 0, 0, 0, 0, 0, 0, 0)
+    msg.linear_acceleration_covariance = (0, 0, 0, 0, 0, 0, 0, 0, 0)
+    rospy.loginfo(msg)
+    pub.publish(msg)
     rate.sleep()
 
 if __name__ == '__main__':
