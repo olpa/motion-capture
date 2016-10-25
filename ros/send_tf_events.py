13945603915727344250479822ec9271b8683eb5
olpa <olpa@uucode.com>
2016-10-25 16:51:57 +0200

    TF messages are being sent, working.

--- ros/send_tf_events.py
+++ ros/send_tf_events.py
@@ -1,32 +1,21 @@
 import rospy
-import std_msgs.msg
-import sensor_msgs.msg
+import tf
 import QuatWithEffect
 
-def set_quat(obj, vals):
-  (obj.x, obj.y, obj.z, obj.w) = vals
-
-def set_vector3(obj, vals):
-  (obj.x, obj.y, obj.z) = vals
-
 def talker():
-  pub = rospy.Publisher('fake_imu', sensor_msgs.msg.Imu, queue_size=10)
+  pub = rospy.Publisher('fake_imu', tf.msg.tfMessage, queue_size=10)
   rospy.init_node('fake_imu', anonymous=True)
   rate = rospy.Rate(10) # 10hz
   quat = QuatWithEffect.QuatWithEffect()
+  br = tf.TransformBroadcaster()
   while not rospy.is_shutdown():
-    msg = sensor_msgs.msg.Imu()
-    set_quat(msg.orientation, quat.next())
-    msg.orientation_covariance = [
-      1.0, 0.0, 0.0,
-      0.0, 1.0, 0.0,
-      0.0, 0.0, 1.0 ]
-    set_vector3(msg.angular_velocity,    (0, 0, 0))
-    set_vector3(msg.linear_acceleration, (0, 0, 0))
-    msg.angular_velocity_covariance    = (0, 0, 0, 0, 0, 0, 0, 0, 0)
-    msg.linear_acceleration_covariance = (0, 0, 0, 0, 0, 0, 0, 0, 0)
-    rospy.loginfo(msg)
-    pub.publish(msg)
+    br.sendTransform(
+      (0, 0, 0),
+      quat.next(),
+      rospy.Time.now(),
+      "imu_sensor1",
+      "world"
+    )
     rate.sleep()
 
 if __name__ == '__main__':
