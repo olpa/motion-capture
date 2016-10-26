3753a110944e46382a0e12ff87319b94ca4cb4fb
Oleg Parashchenko <olpa@uucode.com>
2016-10-26 18:17:02 +0200

    Iniital. Writing a help message.

---
+++ ros/bag_to_xml.py
@@ -0,0 +1,7 @@
+import sys
+if ("-h" in sys.argv) or ("--help" in sys.argv):
+  print "The minimal bag-file dumper. http://wiki.ros.org/Bags/Format/2.0"
+  print "cat file.bag | python bag_to_xml.py >file.xml"
+  sys.exit(0)
+
+print "todo"
