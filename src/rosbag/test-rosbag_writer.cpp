265e21751ff0e81c1e882f43208545645a5c15dc
Oleg Parashchenko <olpa@uucode.com>
2016-11-02 17:43:00 +0100

    The first test passed: writing the initial line.

--- src/rosbag/test-rosbag_writer.cpp
+++ src/rosbag/test-rosbag_writer.cpp
@@ -1,5 +1,42 @@
+#include <string>
+#include <fstream>
+#include <ios>
 #include <gmock/gmock.h>
 using namespace testing;
+
+void write_initial_line(std::ostream& os) {
+  os << "#ROSBAG V2.0\x0a";
+}
+
+class SampleData {
+public:
+  SampleData() {
+    std::stringstream ss;
+    std::ifstream f("../../ros/sample_tf_events_only_2.bag", std::ios_base::in | std::ios_base::binary);
+    ss << f.rdbuf();
+    bytes_ = ss.str();
+  }
+
+  std::string initial_line() const {
+    return bytes_.substr(0, 13);
+  }
+private:
+  std::string bytes_;
+};
+
+SampleData const static_sample;
+
+struct Writer : public Test {
+  SampleData sample = static_sample;
+};
+
+TEST_F(Writer, Writes_Initial_Line) {
+  std::stringstream ss;
+
+  write_initial_line(ss);
+
+  ASSERT_THAT(ss.str(), Eq(sample.initial_line()));
+}
 
 int main(int argc, char** argv) {
   InitGoogleTest(&argc, argv);
